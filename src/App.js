3295ae7ca5473eb8f856e172655e2837ce1df259
mrcysrl <marcysraelmaulion@gmail.com>
2025-01-06 11:09:16 +0800

    email and made with react

--- src/App.js
+++ src/App.js
@@ -3,12 +3,29 @@ import { library } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFacebook } from '@fortawesome/free-brands-svg-icons';
 import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
-library.add(faFacebook);
+library.add(faFacebook, faLinkedin, faEnvelope);
 
 export default function App() {
   const [expandedItem, setExpandedItem] = useState(null);  // tracker
   const [isShrinking, setIsShrinking] = useState(false);   // shrink trigger
+  const [showNotification, setShowNotification] = useState(false); // Notification state
+
+  // Email Copy function
+  const handleCopyEmail = () => {
+    const email = "marcysraelmaulion@gmail.com"; // email addd
+    navigator.clipboard.writeText(email) // copy to clipboard
+      .then(() => {
+        // Show notification
+        setShowNotification(true);
+        // Hide notification after 3 seconds
+        setTimeout(() => {
+          setShowNotification(false);
+        }, 3000);
+      })
+      .catch(err => console.error('Failed to copy email: ', err));
+  };
 
   // Item expansion and transition handle function
   const handleExpand = (item) => {
@@ -22,6 +39,8 @@ export default function App() {
     }, 500); // Duration of the shrink effect
   };
 
+  
+
   return (
     <section className="bento bg-white h-screen flex justify-center items-center">
       <div className="container max-w-screen-7xl p-8 mx-auto my-0 flex justify-center items-center">
@@ -30,33 +49,33 @@ export default function App() {
 
 
 
-{/* Bento Item 1 */}
-<div
-  className={`bento__item cursor-pointer ${expandedItem === "item1" ? "col-span-10 row-span-5 expanded" : "col-span-5 row-span-3"}
-    ${expandedItem && expandedItem !== "item1" ? "hidden" : ""} 
-    ${expandedItem !== "item1" ? "moving-border" : ""}`}
-  onClick={() => handleExpand("item1")}
->
-  {expandedItem === "item1" ? (
-    <div className="expandedContent">Expanded content for Item 1</div>
-  ) : (
-    <div className="relative h-full">
-      <div className="item__title absolute inset-x-0 bottom-0 text-[3rem] font-200 text-center pb-[1rem] text-white drop-shadow-lg font-hammersmith">
-        <h1 className="text-bounce">
-          {"Computer Engineer".split("").map((char, index) => (
-            <span key={index} className="bouncing-letter">
-              {char === " " ? "\u00A0" : char}
-            </span>
-          ))}
-        </h1>
-      </div>
+          {/* Bento Item 1 */}
+          <div
+            className={`bento__item cursor-pointer ${expandedItem === "item1" ? "col-span-10 row-span-5 expanded" : "col-span-5 row-span-3"}
+              ${expandedItem && expandedItem !== "item1" ? "hidden" : ""} 
+              ${expandedItem !== "item1" ? "moving-border" : ""}`}
+            onClick={() => handleExpand("item1")}
+          >
+            {expandedItem === "item1" ? (
+              <div className="expandedContent">Expanded content for Item 1</div>
+            ) : (
+              <div className="relative h-full">
+                <div className="item__title absolute inset-x-0 bottom-0 text-[3rem] font-200 text-center pb-[1rem] text-white drop-shadow-lg font-hammersmith">
+                  <h1 className="text-bounce">
+                    {"Computer Engineer".split("").map((char, index) => (
+                      <span key={index} className="bouncing-letter">
+                        {char === " " ? "\u00A0" : char}
+                      </span>
+                    ))}
+                  </h1>
+                </div>
 
-      <div className="item__image h-full">
-        <img className="w-full h-full object-cover object-[50%_20%] rounded-[1rem]" src="./Marc.webp" alt="" />
-      </div>
-    </div>
-  )}
-</div>
+                <div className="item__image h-full">
+                  <img className="w-full h-full object-cover object-[50%_20%] rounded-[1rem]" src="/Marc.webp" alt="" />
+                </div>
+              </div>
+            )}
+          </div>
 
 
 
@@ -90,9 +109,14 @@ export default function App() {
 
 
           {/* Bento Item 4 */}
-          <div className={`bento__item col-span-3 row-span-1 ${expandedItem && expandedItem !== "item4" ? "hidden" : ""}`}>
-            Item 4
+          <div className={`bento__item col-span-3 row-span-1 flex items-center justify-center items-center bg-[#282c34] rounded-lg shadow-md p-2 gap-[1rem] ${
+              expandedItem === null ? "" : "hidden"}`}>
+            <h3 className="text-[1.5rem] font-bold text-white">Made with ReactJS</h3>
+            <div className="w-16 h-16">
+              <img src="./logo192.png" alt="" className="logo-spin" />
+            </div>
           </div>
+
 
 
 
@@ -146,8 +170,16 @@ export default function App() {
 
 
           {/* Bento Item 9 */}
-          <div className={`bento__item ${expandedItem && expandedItem !== "item9" ? "hidden" : ""}`}>
-            Item 9
+          <div className={`bento__item ${expandedItem && expandedItem !== "item9" ? "hidden" : ""} flex justify-center items-center w-24 h-24 bg-red-600 rounded-full relative`}>
+            <button onClick={handleCopyEmail} className="flex justify-center items-center w-full h-full">
+              <FontAwesomeIcon icon={faEnvelope} size="3x" className="text-white" />
+            </button>
+            {/* Notification bubble */}
+            {showNotification && (
+              <div className="absolute top-[-2.5rem] left-1/2 transform -translate-x-1/2 bg-green-500 text-white px-[2rem] py-[1rem] rounded-[1rem] text-sm shadow-lg text-center max-w-[40rem] whitespace-nowrap">
+                Email Address Copied
+              </div>
+            )}
           </div>
 
 
